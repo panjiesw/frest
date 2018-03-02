6c3bf8df1947b941bf889b256d3dd5cbfd7ffe7a
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2018-03-02 16:00:38 +0700

    test(json): complete test

--- packages/frest-json/src/error.ts
+++ packages/frest-json/src/error.ts
@@ -10,17 +10,19 @@ export interface IJSONErrorOption {
   headerContent?: string;
 }
 
-const error = (opts: IJSONErrorOption) => {
+const error = (opts: IJSONErrorOption = {}) => {
   const jsonErrorInterceptor: f.IErrorInterceptor = err =>
     new Promise<f.IResponse<any> | null>((resolve, reject) => {
       const { headerContent } = opts;
-      const { response } = err;
+      const { response, request } = err;
+      const { skip } = request;
       if (response) {
         const { headers, bodyUsed } = response.origin;
         const ct = headers.get('Content-Type');
         if (
           ct &&
           ct.indexOf(headerContent || 'application/json') >= 0 &&
+          (!skip || skip.indexOf(ID_ERROR) < 0) &&
           !bodyUsed
         ) {
           response.origin.json().then(body => {
