26944a9fea0ba8f5f0251301d33b992e4ea7c21c
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2019-10-16 00:49:12 +0700

    refactor!: remove monorepo files
    BREAKING CHANGE: frest now use babel to compile its sources. There are also changes internally in how async loops are done.

--- internal/docs/content/api/_index.en.md
+++
@@ -1,8 +0,0 @@
----
-title: API Reference
-pre: "<b>3. </b>"
-weight: 15
----
-
-* `frest` module [API Reference](frest/)
-* `frest-json` module [API Reference](frest-json/)
--- internal/test-helpers/index.js
+++
@@ -1,8 +0,0 @@
-const nodeFetch = require('node-fetch');
-
-if (!global.fetch) {
-  global.fetch = nodeFetch;
-  global.Response = nodeFetch.Response;
-  global.Headers = nodeFetch.Headers;
-  global.Request = nodeFetch.Request;
-}
